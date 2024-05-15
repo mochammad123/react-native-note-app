053224c3a524603c1207f2f4835da5d1ad87fe14
Faisal <it13.knitto@gmail.com>
2024-05-15 09:29:58 +0700

    React native note app

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import Home from "./src/screens/home";
+import AddNote from "./src/screens/addNote";
+import EditNote from "./src/screens/editNote";
 
-export default function App() {
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+  deleteNote,
+  setDataNote,
+  dataNote,
+  editNote,
+}) => {
+  switch (currentPage) {
+    case "home":
+      return (
+        <Home
+          noteList={noteList}
+          setCurrentPage={setCurrentPage}
+          deleteNote={deleteNote}
+          setDataNote={setDataNote}
+        />
+      );
+    case "add":
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+    case "edit":
+      return (
+        <EditNote
+          setCurrentPage={setCurrentPage}
+          dataNote={dataNote}
+          editNote={editNote}
+        />
+      );
+    default:
+      return <Home />;
+  }
+};
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState("home");
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: "Note Pertama",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    },
+  ]);
+  const [dataNote, setDataNote] = useState();
+
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ]);
+  };
+
+  const editNote = (id, title, desc) => {
+    setNoteList(
+      noteList.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            title: title,
+            desc: desc,
+          };
+        } else {
+          return note;
+        }
+      })
+    );
+    setCurrentPage("home");
+  };
+
+  const deleteNote = (id) => {
+    setNoteList(noteList.filter((note) => note.id !== id));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CurrentPageWidget
+      noteList={noteList}
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      addNote={addNote}
+      deleteNote={deleteNote}
+      dataNote={dataNote}
+      editNote={editNote}
+      setDataNote={setDataNote}
+    />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
